b8b1cafa02cfb0ad14282ac5928fd6c445a3e08c
Anrey <naumovandreyn@gmail.com>
2021-12-05 22:52:03 +0300

    FirstSave

---
+++ ClientUDP/Program.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace ApplicationUDP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string serverIp;
+                {
+                    Console.Write("Введите ip сервера: ");
+                    serverIp = Console.ReadLine();
+                }
+                int clientPort = 8002, serverPort;
+                {
+                    Console.Write("Введите номер порта сервера (8001): ");
+                    serverPort = Convert.ToInt32(Console.ReadLine());
+                }
+
+                var udpEndPoint = new IPEndPoint(IPAddress.Parse(serverIp), clientPort);
+                var udpSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+                udpSocket.Bind(udpEndPoint);
+
+                while (true)
+                {
+                    try
+                    {
+                        byte codeOperation;
+                        {
+                            Console.WriteLine("1 - Найти среднее арифметическое чисел файла");
+                            Console.WriteLine("2 - Написать сообщение");
+                            Console.WriteLine("3 - Выйти");
+                            Console.Write("Введите код операции: ");
+                            codeOperation = Convert.ToByte(Console.ReadLine());
+                        }
+
+                        if (codeOperation == 1 || codeOperation == 2 || codeOperation == 3)
+                        {
+                            var serverEndPoint = new IPEndPoint(IPAddress.Parse(serverIp), serverPort);
+                            udpSocket.SendTo(new byte[] { codeOperation }, serverEndPoint);
+
+                            if (codeOperation == 1)
+                            {
+                                byte[] data;
+                                {
+                                    Console.Write("Путь к файлу: ");
+                                    var path = Console.ReadLine();
+                                    data = Encoding.UTF8.GetBytes(path);
+                                }
+                                udpSocket.SendTo(data, serverEndPoint);
+
+                                StringBuilder fileText = GetMessage(udpSocket);
+                                Console.WriteLine("Среднее арифметическое чисел файла = " + ArithmeticMean(fileText));
+                            }
+                            else if (codeOperation == 2)
+                            {
+                                byte[] data;
+                                {
+                                    Console.Write("Введите сообщение: ");
+                                    string message = Console.ReadLine();
+                                    data = Encoding.UTF8.GetBytes(message);
+                                }
+                                udpSocket.SendTo(data, serverEndPoint);
+                            }
+                            else if (codeOperation == 3)
+                            {
+                                udpSocket.Shutdown(SocketShutdown.Both);
+                                udpSocket.Close();
+                                break;
+                            }
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private static StringBuilder GetMessage(Socket listener)
+        {
+            byte[] buffer = new byte[256];
+            int messageSize;
+            StringBuilder data = new StringBuilder();
+
+            EndPoint senderEndPoint = new IPEndPoint(IPAddress.Any, 0);
+
+            do
+            {
+                messageSize = listener.ReceiveFrom(buffer, ref senderEndPoint);
+                data.Append(Encoding.UTF8.GetString(buffer, 0, messageSize));
+            }
+            while (listener.Available > 0);
+            return data;
+        }
+
+        private static double ArithmeticMean(StringBuilder fileText)
+        {
+            fileText.Append("\n");
+
+            Regex regex = new Regex(@"(.*?)\n");
+            MatchCollection matchCollection = regex.Matches(fileText.ToString());
+
+            double arithmeticMean = default(double);
+            if (matchCollection.Count > 0)
+            {
+                try
+                {
+                    for (int i = 0; i < matchCollection.Count; i++)
+                    {
+                        arithmeticMean += Convert.ToDouble(matchCollection[i].Groups[1].Value.Trim());
+                    }
+                    arithmeticMean /= matchCollection.Count;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+            return arithmeticMean;
+        }
+    }
+}
---
+++ ServerUDP/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace ApplicationUDP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int port = 8001;
+            IPAddress ip = GetLocalIP();
+            var udpEndPoint = new IPEndPoint(ip, port);
+
+            var udpSocet = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
+            try
+            {
+                udpSocet.Bind(udpEndPoint);
+
+                WriteWithTime("Сервер запущен. Ожидание подключений...");
+                while (true)
+                {
+                    IPEndPoint senderIp;
+                    EndPoint senderEndPoin;
+                    byte codeOperation;
+                    {
+                        var size = 0;
+                        byte[] buffer = new byte[1];
+
+                        senderEndPoin = new IPEndPoint(IPAddress.Any, 0);
+
+                        size = udpSocet.ReceiveFrom(buffer, ref senderEndPoin);
+                        senderIp = senderEndPoin as IPEndPoint;
+
+                        codeOperation = buffer[0];
+                    }
+
+                    //WriteWithTime("Подключение установленно");
+
+                    if (codeOperation == 1 || codeOperation == 2 || codeOperation == 3)
+                    {
+                        if (codeOperation == 1)
+                        {
+                            string filePath = GetMessage(udpSocet).ToString();
+
+                            WriteWithTime(senderIp.Address.ToString() + ": Запрос на обработку файла: " + filePath);
+
+                            byte[] array;
+                            using (FileStream fStriam = new FileStream(filePath, FileMode.Open))
+                            {
+                                array = new byte[fStriam.Length];
+                                fStriam.Read(array, 0, array.Length);
+                            }
+                            udpSocet.SendTo(array, senderEndPoin);
+                        }
+                        else if (codeOperation == 2)
+                        {
+                            WriteWithTime(senderIp.Address.ToString() + ": " + GetMessage(udpSocet).ToString());
+                        }
+                        else if (codeOperation == 3)
+                        {
+                            WriteWithTime(senderIp.Address.ToString() + ": " + "Клиент отключен");
+                            //udpSocet.Shutdown(SocketShutdown.Both);
+                            //udpSocet.Close();
+
+                            WriteWithTime("Ожидание подключений...");
+                        }
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private static StringBuilder GetMessage(Socket listeningSocket)
+        {
+            byte[] buffer = new byte[256];
+            int messageSize;
+            StringBuilder data = new StringBuilder();
+
+            EndPoint senderEndPoin = new IPEndPoint(IPAddress.Any, 0);
+            do
+            {
+                messageSize = listeningSocket.ReceiveFrom(buffer, ref senderEndPoin);
+                data.Append(Encoding.UTF8.GetString(buffer, 0, messageSize));
+            }
+            while (listeningSocket.Available > 0);
+
+            return data;
+        }
+
+        private static IPAddress GetLocalIP()
+        {
+            using (Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, 0))
+            {
+                socket.Connect("8.8.8.8", 65530);
+                IPEndPoint endPoint = socket.LocalEndPoint as IPEndPoint;
+                return endPoint.Address;
+            }
+        }
+
+        private static void WriteWithTime(string str)
+        {
+            Console.WriteLine(DateTime.Now.ToShortTimeString() + ": " + str);
+        }
+    }
+}
